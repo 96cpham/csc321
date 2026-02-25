0146dd743610a1398a3522f6af1b5c4c86d0a480
96cpham <96cpham@gmail.com>
2026-02-25 01:38:30 +0000

    lab 7 completed

---
+++ cpham7.py
@@ -0,0 +1,20 @@
+
+
+x = 6
+y = 6.7
+z = "carrot"
+
+if (x > 7): 
+        print("x is greater than 7")
+else:
+    print("x is less than 7")
+
+if (y < 8.9):
+    print("y is less than 8.9")
+else:
+    print("y is less than 8.9")
+
+if (z == "onion"):
+    print("z is an onion")
+else:
+    print("z is not an onion")
